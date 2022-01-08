1438e9927d14aeac350fc5bab2830866d0d959ec
lulor <lorenzo.lu@mailbox.org>
2022-01-08 22:28:07 +0100

    Fix epoch and cache count

--- train.py
+++ train.py
@@ -53,20 +53,21 @@ else:   # adam
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 criterion_triplet = nn.TripletMarginLoss(margin=args.margin, p=2, reduction="sum")
 
-best_r5 = 0
-not_improved_num = 0
+if args.resume_model is None:
+    epoch_num = 0
+    best_r5 = 0
+    not_improved_num = 0
+# Resume model
+else:
+    epoch_num, recalls, best_r5 = util.recover_from_state(args.resume_model, model, optimizer)
+    logging.info(f"Successfully loaded model (epoch: {epoch_num}, recalls: {recalls})")
 
 logging.info(f"Output dimension of the model is {args.features_dim}")
 
 #### Training loop
 for epoch_num in range(args.epochs_num):
 
-    # Resume model
-    if args.resume_model is not None:
-        epoch_num, recalls, best_r5 = util.recover_from_state(args.resume_model, model, optimizer)
-        logging.info(f"Successfully loaded model (epoch: {epoch_num}, recalls: {recalls})")
-        args.resume_model = None
-        epoch_num += 1
+    epoch_num += 1
 
     logging.info(f"Start training epoch: {epoch_num:02d}")
     
@@ -76,7 +77,7 @@ for epoch_num in range(args.epochs_num):
     # How many loops should an epoch last (default is 5000/1000=5)
     loops_num = math.ceil(args.queries_per_epoch / args.cache_refresh_rate)
     for loop_num in range(loops_num):
-        logging.debug(f"Cache: {loop_num} / {loops_num}")
+        logging.debug(f"Cache: {loop_num + 1} / {loops_num}")
         
         # Compute triplets to use in the triplet loss
         triplets_ds.is_inference = True
