3ee309b7ad0b8306253bb368ac11c52aa1682bfd
Guido Genco <50440306+Guido27@users.noreply.github.com>
2022-01-31 10:43:06 +0100

    fix concatenations

--- train.py
+++ train.py
@@ -129,12 +129,13 @@ if not args.test_only:
                 negative_features = torch.Tensor().to(args.device)
 
                 for queri in queries_indexes:
-                    queri_features = queri_features.cat(features[queri])
+                    queri_features = torch.cat((queri_features,features[queri])) #cat as column
                 for positive in positives_indexes:
-                    positive_features = positive_features.cat(features[positive])
+                    positive_features = torch.cat((positive_features,features[positive])) #cat as column
                 for negative in negatives_indexes:
-                    negative_features = negative_features.cat(features[negative])
-                output_features= output_features.cat((queri_features,positive_features,negative_features),1)
+                    negative_features = torch.cat((negative_features, features[negative])) #cat as column
+                #output_features should be a tensor made by 3 columns: queries,positive,negative features, in this way each row is a triplet    
+                output_features= torch.cat((output_features,queri_features,positive_features,negative_features),1) 
                 loss += sare_loss.get_loss(output_features,'sare_joint',args.train_batch_size,3)
                     
                     
