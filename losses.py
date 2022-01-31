1740cc8a3b990d5f02b43eeb7552b7fef3ada7e1
lulor <lorenzo.lu@mailbox.org>
2022-01-31 01:32:19 +0100

    Try to fix SARE loss

--- losses.py
+++ losses.py
@@ -46,7 +46,4 @@ class SAREJointLoss(nn.Module):
     def forward(self, anchors, positives, negatives):
         dist_pos = torch.sum(torch.pow(anchors - positives, 2), dim=1)
         dist_neg = torch.sum(torch.pow(anchors - negatives, 2), dim=1)
-
-        dist = -torch.cat((dist_pos, dist_neg))
-        dist = F.log_softmax(dist, dim=0)
-        return -dist.mean()
+        return torch.sum(dist_pos + torch.logaddexp(-dist_pos, -dist_neg)).mean
