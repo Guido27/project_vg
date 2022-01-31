332466f67eb64aa995d5f7fe2a58dcca7a3afd70
lulor <lorenzo.lu@mailbox.org>
2022-01-31 12:10:10 +0100

    Add more stable torch method method for sare loss

--- losses.py
+++ losses.py
@@ -23,8 +23,7 @@ class TripletLoss(nn.Module):
         # anchors, positives and negatives are N x D (N: number of images, D: dimensionality)
         dist_pos = torch.sum(torch.pow(anchors - positives, 2), dim=1)
         dist_neg = torch.sum(torch.pow(anchors - negatives, 2), dim=1)
-        nq = anchors.size(dim=0)
-        return torch.sum(torch.clamp(dist_pos - dist_neg + self.margin, min=0)) / nq
+        return torch.mean(torch.clamp(dist_pos - dist_neg + self.margin, min=0))
 
 
 class SOSLoss(nn.Module):
@@ -46,7 +45,11 @@ class SAREIndLoss(nn.Module):
     def forward(self, anchors, positives, negatives):
         dist_pos = torch.sum(torch.pow(anchors - positives, 2), dim=1)
         dist_neg = torch.sum(torch.pow(anchors - negatives, 2), dim=1)
-        c_pq = torch.exp(-dist_pos) / (torch.exp(-dist_pos) + torch.exp(-dist_neg))
-        batch_loss = -torch.log(c_pq)
-        nq = anchors.size(dim=0)
-        return torch.sum(batch_loss) / nq
+
+        # c_pq = torch.exp(-dist_pos) / (torch.exp(-dist_pos) + torch.exp(-dist_neg))
+        # batch_loss = -torch.log(c_pq)
+        # nq = anchors.size(dim=0)
+        # return torch.sum(batch_loss) / nq
+
+        # OR
+        return torch.mean(dist_pos + torch.logaddexp(-dist_pos, -dist_neg))
