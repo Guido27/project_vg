40552fa4cee40b44ec37238f9543c2e327ef494c
lulor <lorenzo.lu@mailbox.org>
2022-02-07 12:28:03 +0100

    Formatting

--- netvlad.py
+++ netvlad.py
@@ -25,10 +25,12 @@ class NetVLAD(nn.Module):
         clsts_assign = clsts / np.linalg.norm(clsts, axis=1, keepdims=True)
         dots = np.dot(clsts_assign, traindescs.T)
         dots.sort(0)
-        dots = dots[::-1, :] # sort, descending
-        alpha = (-np.log(0.01) / np.mean(dots[0,:] - dots[1,:])).item()
+        dots = dots[::-1, :]  # sort, descending
+        alpha = (-np.log(0.01) / np.mean(dots[0, :] - dots[1, :])).item()
         self.centroids = nn.Parameter(torch.from_numpy(clsts))
-        self.conv.weight = nn.Parameter(torch.from_numpy(alpha * clsts_assign).unsqueeze(2).unsqueeze(3))
+        self.conv.weight = nn.Parameter(
+            torch.from_numpy(alpha * clsts_assign).unsqueeze(2).unsqueeze(3)
+        )
         self.conv.bias = None
 
     def forward(self, x):
@@ -39,14 +41,19 @@ class NetVLAD(nn.Module):
         soft_assign = F.softmax(soft_assign, dim=1)
 
         x_flatten = x.view(N, C, -1)
-        
+
         # calculate residuals to each clusters
-        vlad = torch.zeros([N, self.num_clusters, C], dtype=x.dtype, layout=x.layout, device=x.device)
-        for C in range(self.num_clusters): # slower than non-looped, but lower memory usage 
-            residual = x_flatten.unsqueeze(0).permute(1, 0, 2, 3) - \
-                    self.centroids[C:C+1, :].expand(x_flatten.size(-1), -1, -1).permute(1, 2, 0).unsqueeze(0)
-            residual *= soft_assign[:,C:C+1,:].unsqueeze(2)
-            vlad[:,C:C+1,:] = residual.sum(dim=-1)
+        vlad = torch.zeros(
+            [N, self.num_clusters, C], dtype=x.dtype, layout=x.layout, device=x.device
+        )
+        for C in range(
+            self.num_clusters
+        ):  # slower than non-looped, but lower memory usage
+            residual = x_flatten.unsqueeze(0).permute(1, 0, 2, 3) - self.centroids[
+                C : C + 1, :
+            ].expand(x_flatten.size(-1), -1, -1).permute(1, 2, 0).unsqueeze(0)
+            residual *= soft_assign[:, C : C + 1, :].unsqueeze(2)
+            vlad[:, C : C + 1, :] = residual.sum(dim=-1)
 
         vlad = F.normalize(vlad, p=2, dim=2)  # intra-normalization
         vlad = vlad.view(x.size(0), -1)  # flatten
